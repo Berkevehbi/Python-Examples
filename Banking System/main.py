e4ee172055d4b4825447fb067434bf291e885630
Berkevehbi <berkevehbi@gmail.com>
2023-12-05 22:48:02 +0300

    Class created

---
+++ Banking System/main.py
@@ -0,0 +1,39 @@
+import datetime
+
+
+class User:
+    def __init__(self, full_name, balance):
+        self.__balance = balance
+        self.full_name = full_name
+
+    def deposit(self, money_amount):
+        self.__balance += money_amount
+
+    def withdraw(self, money_amount):
+        if self.__balance - money_amount >= 0:
+            self.__balance -= money_amount
+            print("{} dollar has been withdrawn.".format(money_amount))
+            self.save_the_process_withdraw(money_amount)
+        else:
+            print("The user has less money and cannot withdraw that much money.")
+
+    def write_balance(self):
+        print(f'The {self.full_name}\'s money is {self.__balance} dollar.')
+
+    def edit_name(self, new_full_name):
+        self.full_name = new_full_name
+
+    def save_the_process_withdraw(self, money_amount):
+        with open("database.txt", "a") as file:
+            file.write("{} withdrawed {} at {}\n".format(self.full_name, money_amount,
+                                                         datetime.datetime.now().strftime("%Y-%m-%d %H:%M")))
+
+def create_account(full_name):
+    return User(full_name, 0)
+
+
+user1 = create_account("Vehbi Berke")
+user1.deposit(150)
+user1.withdraw(20)
+
+user1.write_balance()
